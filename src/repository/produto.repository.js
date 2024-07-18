ed28e0643cf24122be4528e98bd3ec01733db7b2
Matheus Da Silva Oliveira <matheus9926@digitalcollegesul.local>
2024-07-18 16:56:29 -0300

    api dripstore

---
+++ src/repository/produto.repository.js
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient;
+
+export const getAll = async () => {
+ return await prisma.produtos.findMany();
+}
+
+export const getOne = async (id) => {
+    return await prisma.produtos.findUnique(
+        {
+            where:
+            {
+                id : Number(id)
+            }
+        });
+   }
+
+export const create = async (produtos) => {
+    return await prisma.produtos.create({data: produtos});
+}
+
+export const update = async (id, produto) => {
+    return await prisma.produtos.update({
+            where: { id: Number(id) },
+            data: produto,
+        });
+   };
+
+export const deletar = async (id) => {
+   return await prisma.produtos.delete(
+        {
+            where:
+            {
+                id : Number(id)
+            }
+        });
+   }
